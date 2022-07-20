41ad0adfd8d10fb7b8b715b1bd196a42a261af87
BGMloop <101558873+BGMloop@users.noreply.github.com>
2022-07-20 11:39:21 -0500

    Pages update
    Worked on pages: login, register, dashboard,

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -4,7 +4,7 @@ import Logo from "./../assets/foto-jets.png";
 
 // formit
 import { Formik } from 'formik';
-import { Form } from "formik";
+import { Link } from "react-router-dom";
 import { TextInput } from "../components/FormLib";
 import * as Yup from 'yup';
 
@@ -16,51 +16,36 @@ import loader from 'react-loader-spinner';
 
 const Register = () => {
     return (
-        <div>
-            <StyledFormArea>
-                <avatar image={Logo} />
-                <StyledTitle color={colors.theme} size={30}>memeber register</StyledTitle>
-                <Formik>
-                    initialValues ={{
-                        email: "",
-                        Password: "",
-                        repeatPassword: "",
-                        dateofbirth: "",
-                        name: ""
-                    }}
-                    validationSchema={Yup.object({
-                    dateOfBirth: Yup.date().required("Required"),
-                    email: Yup.string().email("Invalid email address").required("Required"),password: Yup.string().min(8, "Password is too short").max(30, "Password is too long").required("Required"), name: Yup.string().reqired("Required"), dateOfBirth: Yup.date().required("Required"), repeatPassword: Yup.string().required("Required").oneOf([Yup.ref("password")], "Passwords must match")
-                    })
-                }
-                onSubmit={(values, {setSubmitting}) => { console.log(values);
-                }}
-                {({isSubmitting}) => (
-                    <Form>
-                        <TextInput name="name" type="text" labe1="Full Name" placeholder="Gasman Simpson" icon={<FiUser/>} />
-                        <TextInput name="email" type="text" labe1="Email Address" placeholder="gasman1@example.com"
-                        icon={<FiMail/>}/>
-                        <TextInput name="dateOfBirt" type="text" labe1="Date of Birth" icon={<FiCalendar/>}/>
-                        <TextInput name="password"
-                        type="password"
-                        labe1="Password"
-                        placeholder="********" icon={<FiLock/>}/>
-                        <TextInput name="repeatpassword" type="password" labe1="Repeat Password" placeholder="********" icon={<FiLock/>}/>
-                        <ButtonGroup>
-                            {isSubmitting && ( <StyledFormButton type="submit">Register</StyledFormButton>)}
-                            
-                        </ButtonGroup>
-                    </Form>
-                )}
-                </Formik>
-                <ExtraText>
-                    Already have an account? <TextLink to='/login'>Login</TextLink> 
-                </ExtraText>
-            </StyledFormArea>
-            <CopyrightText>
-                All right reserved &copy;2020
-            </CopyrightText>
+		<div class="flex justify-center w-screen h-screen">
+            <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 w-[600px] h-[300px]">
+            <div className='text-8xl'></div>
+                {/* //background */}
+                <img src="https://images.pexels.com/photos/302743/pexels-photo-302743.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" class="w-[4000px] h-auto" alt="..." />
+                <div class="mb-4">
+                    <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+                        Username
+                    </label>
+                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username" />
+                </div>
+                <div class="mb-6">
+                    <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+                        Password
+                    </label>
+                    <input class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="Enter password" />
+                    <p class="text-red-500 text-xs italic">Please choose a password.</p>
+                </div>
+                <div class="justify-center mr-10">
+                <Link to="/">
+                    <button class="bg-blue-500 mr-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+                        Sign In
+                    </button>
+                    </Link>
+                    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+                        Register
+                    </button>
+                </div>
+            </form>
         </div>
-    )
+	)
 }
 export default Register;
