aa807564592cd83ac4cdef8458c11d2480bbe1e4
DajanekD <82248461+DajanekD@users.noreply.github.com>
2022-07-27 13:43:34 -0500

    Update Login/Register

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,45 +1,30 @@
-
-import Logo from "./../assets/foto-jets.png";
-
-// formit
-import {Formik, Form} from 'formik';
 import { Link } from "react-router-dom";
-import * as Yup from 'yup';
-
-// icons
-import {FiMail, FiLock}from 'react-icons/fi';
+import React from 'react'
+import FitImage from './FitImage'
+import './LoginS.css'
 
 //loader 
 import Loader from 'react-loader-spinner';
 import Navbar from "./Navbar";
 
-const Login = async () => {
+const Login = () => {
     return (
-        <div className="min-h-screen bg-gray-200 text-gray-700 antialiased py-6 flex-col justify-center sm:py-12">
-            <div className="relative py-3 sm:max-w-xl mx-auto text-center">
-                <span className="text-2xl font-light">Login to your account</span>
-                 <div className="relative mt-4 bg-white shadow-orange-500 sm:rounded-lg text-left">
-                    <div className="h-2 bg-indigo-400 rounded-t-md"></div>
-                    <div className="py-6 px-8">
-                        <label className="block mt-4 font-semibold text-black">Username or Email</label>
-                        <input type="text" placeholder="Username" className="border w-full h-4 px-32 py-2 mt-2 hover:outline-none focus:ring-2 focus:ring-indigo-700 rounded-md"/>
-                        <label className="block mt-4 font-semibold text-black">Password</label>
-                        <input type="password" placeholder="Password" className="border w-full h-4 px-32 py-2 mt-2 hover:outline-none focus:ring-2 focus:ring-indigo-700 rounded-md"/>
-                        <div className="justify-center mr-10">
-                        <Link to="/dashboard">
-                            <button class="mt-5 bg-indigo-700 mr-4 text-white py-2 px-6 rounded-lg">Login</button>
-                            </Link>
-                            <Link to="/register">
-                            <button className="mt-5 bg-indigo-700 text-white py-2 px-6 rounded-lg">Register</button>
-                            </Link>
-                        </div>
-                    </div>
-                </div>
-            </div>    
-        </div>
-           
-
-    );
-}
-
-export default Login;
+      <div>
+          <Navbar />
+          <FitImage heading='LOG.' text='IN '/>
+  
+          <div className='login'>
+          <h1>User Login</h1>
+          <label>Username</label>
+          <input type='text' />
+          <label>Password</label>
+          <input type='text' />
+          <button className='btn'>Login</button>
+  
+          </div>
+  
+      </div>
+    )
+  }
+  
+  export default Login
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,13 +1,9 @@
 import {StyledTextInput, StyledFormArea, StyledFormButton, StyledLabe1, Avatar, StyledTitle, colors, ButtonGroup, ExtraText, TextLink, CopyrightText} from "../components/Styles";
 
-import Logo from "./../assets/foto-jets.png";
-
-// formit
-import { Formik } from 'formik';
 import { Link } from "react-router-dom";
 import { TextInput } from "../components/FormLib";
 import * as Yup from 'yup';
-
+import FitImage from './FitImage'
 // icons
 import { FiMail, FiLock, FiUser, FiCalendar }from 'react-icons/fi';
 
@@ -15,62 +11,29 @@ import { FiMail, FiLock, FiUser, FiCalendar }from 'react-icons/fi';
 import loader from 'react-loader-spinner';
 import Navbar from "./Navbar";
 
-const Register = async () => {
+const Register = () => {
     return (
         <div>
-            <Navbar />
-            <div />
-		<div class="flex justify-center w-screen h-screen">
-            <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 w-[600px] h-[300px]">
-            <div className='text-8xl'></div>
-                {/* //background */}
-                <img src="https://images.pexels.com/photos/302743/pexels-photo-302743.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" class="w-[4000px] h-auto" alt="..." />
-                <div class="mb-4">
-                    <label class="block text-gray-700 text-sm font-bold mb-2" for="email">
-                        Email
-                    </label>
-                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username" />
-                </div>
-                <div class="mb-4">
-                    <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
-                        Username
-                    </label>
-                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username" />
-                </div>
-                <div class="mb-6">
-                    <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
-                        Password
-                    </label>
-                    <input class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="Enter" />
-                    <p class="text-red-500 text-xs italic">Please choose a password.</p>
-                </div>
-                <div class="justify-center mr-10">
-                <Link to="/">
-                    <button class="bg-blue-500 mr-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
-                        Sign In
-                    </button>
-                    </Link>00
+        <Navbar />
+        <FitImage heading='JOIN.' text='US '/>
+
+    <div className='registration'>
+        <h1>User Registration</h1>
+      <form >
+        <label>Username</label>
+        <input type='text' name='user_name' placeholder= 'username' />
+        <label>Email</label>
+        <input type='text' name='email' placeholder='email' />
+        <input type='text' name= 'password' placeholder='password' />
+        <button className='btn'>Register</button>
+      </form>
+    </div>
+
+    </div>
 
 
 
 
-
-
-
-
-
-
-
-
-
-                    3.3...............................
-                    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
-                        Register
-                    </button>
-                </div>
-            </form>
-        </div>
-        </div>
 	)
 }
 export default Register;
