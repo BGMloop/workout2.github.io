42b3409443fa447830aaa45a756a9ea1d67716af
BGMloop <101558873+BGMloop@users.noreply.github.com>
2022-07-31 20:26:16 -0500

    about page fix style

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,21 +4,27 @@ import FitImage from './FitImage'
 import './AboutS.css'
 
 
+
 const About = () => {
   return (
-    <div>
-        <Navbar />
-        <FitImage heading='ABOUT.' text='US '/>
-        <div className= 'content'> 
-        <h5>
-        Build Your Body Strong<span> & Feel Good About Yourself</span></h5>
-        <p>
-        Our goal is to help create a better, happier, healthier life. 
-        We want you to have a great member experience with our website. We believe anything is possible when hard work is involved. Our website is designed to build the perfect body in which you deserve to see.
-        </p>
-        <button className="button">Lets Talk </button>
-    </div>
-    </div>
+    <body>
+      <Navbar />
+      <section class="about">
+        <div class="FitMan">
+          <FitImage src="img/FitMan.jpg" />
+          <div class="about-text" >
+            <h1>About Us</h1>
+            <h5>
+            Build Your Body Strong<span> & Feel Good About Yourself</span></h5>
+            <p>
+            Our goal is to help create a better, happier, healthier life. 
+            We want you to have a great member experience with our website. We believe anything is possible when hard work is involved. Our website is designed to build the perfect body in which you deserve to see.
+            </p>
+            <button className="button">Lets Talk </button>
+         </div>
+        </div>
+        </section>
+    </body>
   )
 }
 
